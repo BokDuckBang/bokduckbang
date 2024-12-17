56b7bdd106f3f1e23aa7a00172a098b4b0a900e8
Ami Ryu <directorryu28@gmail.com>
2024-12-17 22:49:18 +0900

    Update devvit label

--- cat-racing/src/main.tsx
+++ cat-racing/src/main.tsx
@@ -94,7 +94,7 @@ const createRaceForm = Devvit.createForm(
 
 Devvit.addMenuItem({
   // Please update as you work on your idea!
-  label: '[Cat Racing] Make cat racing game - ami test',
+  label: '[Cat Racing] Make cat racing game',
   location: 'subreddit',
   forUserType: 'moderator',
   onPress: async (_event, context) => {
